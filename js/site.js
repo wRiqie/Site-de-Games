dab95c174cb02037eeb1b6e27927874594f21f4f
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2020-12-14 16:57:39 -0300

    Arquivos
    Arquivos do site.

---
+++ js/site.js
@@ -0,0 +1,15 @@
+function go_to_detalhes(){
+    window.location="detalhes.html"
+}
+
+function go_to_home(){
+    window.location="index.html"
+}
+
+function go_to_admin(){
+    window.location="area-administrativa.html"
+}
+
+function go_to_cad(){
+    window.location="cadastro-produtos.html"
+}
